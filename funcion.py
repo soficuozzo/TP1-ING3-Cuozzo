414e864c13ead7f8609b6ac6afe4b185f7bc1016
Sofia Cuozzo <2313251@ucc.edu.ar>
2025-08-22 09:37:28 -0300

    Agrego funcion sumar dos numeros

---
+++ funcion.py
@@ -0,0 +1,7 @@
+def sumar(a, b):
+    return a + b
+if __name__ == "__main__":
+    num1 = int(input("Ingresá el primer número: "))
+    num2 = int(input("Ingresá el segundo número: "))
+    resultado = sumar(num1, num2)
+    print(f"La suma es: {resultado}")
