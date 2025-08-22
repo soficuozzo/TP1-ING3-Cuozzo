12191b44871bfea4a126cab76211c9f389ec18c4
Sofia Cuozzo <2313251@ucc.edu.ar>
2025-08-22 09:42:14 -0300

    Modifico funcion sumar. Ahora suma 3 numeros

--- funcion.py
+++ funcion.py
@@ -1,7 +1,8 @@
-def sumar(a, b):
-    return a + b
+def sumar(a, b, c):
+    return a + b + c
 if __name__ == "__main__":
     num1 = int(input("Ingresá el primer número: "))
     num2 = int(input("Ingresá el segundo número: "))
-    resultado = sumar(num1, num2)
+    num3 = int(input("Ingresá el tercer número: "))
+    resultado = sumar(num1, num2, num3)
     print(f"La suma es: {resultado}")
